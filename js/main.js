a7d770adb655052f392134adac380ed96a98720c
Nahuel Escudero <escuderogomezn@gmail.com>
2023-10-04 19:39:01 -0300

    Arreglos puntuales para el funcionamiento de los while y switch

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-let opc = Number(prompt("Que categoria le interesa?: 1- Deportes y Fitness _ 2- Accesorios de Vehiculos _ 3- Industrias y Oficinas _ 4- SALIR"));
 
 let stockDepYFit = [
     {nombreProd: "Pelota Cobra Futsal", stock: 0},
@@ -26,72 +25,91 @@ const verificarStock = producto => {
         
     } else {
         alert(`${producto.nombreProd} comprado exitosamente`);
+        let reCompra = Number(prompt("Desea comprar algo mas? 1-SI _ 2-NO"));
+        switch(reCompra) {
+            case 1: 
+                compra();
+                break;
+            case 2:
+                op, op1, op2 = 4;
+                break;
+            default:
+                alert("Ingrese una opcion valida");
+        }
     }
 }
 
-while(opc != 4){
-    switch(opc){
-        case 1:
-            let op = Number(prompt("Que articulo desea comprar?: 1. Pelota _ 2. Guantes de boxeo _ 3. Skate"));
-            while(op != 4) {
-                switch(op) {
-                    case 1:
-                        verificarStock(stockDepYFit[0]);
-                        break;
-                    case 2:
-                        verificarStock(stockDepYFit[1]);
-                        break;
-                    case 3:
-                        verificarStock(stockDepYFit[2]);
-                        break;
-                    case 4:
-                        break;
-                    default:
-                        alert("Por favor ingrese un numero valido");
+const compra = () => {
+    let opc = Number(prompt("Que categoria le interesa?: 1- Deportes y Fitness _ 2- Accesorios de Vehiculos _ 3- Industrias y Oficinas _ 4- SALIR"));
+
+    while(opc != 4){
+        switch(opc){
+            case 1: 
+                let op = Number(prompt("Que articulo desea comprar?: 1. Pelota _ 2. Guantes de boxeo _ 3. Skate"));
+                while(op != 4) {
+                    switch(op) {
+                        case 1:
+                            verificarStock(stockDepYFit[0]);
+                            break;
+                        case 2:
+                            verificarStock(stockDepYFit[1]);
+                            break;
+                        case 3:
+                            verificarStock(stockDepYFit[2]);
+                            break;
+                        case 4:
+                            break;
+                        default:
+                            alert("Por favor ingrese un numero valido");
+                    }
                 }
-            }
-        case 2:
-            let op1 = Number(prompt("Que articulo desea comprar?: 1. Cable Espiral 0K051 _ 2. Cable Espiral EV06E _ 3. Cable Espiral 12070"));
-            while(op1 != 4) {
-                switch(op1) {
-                    case 1:
-                        verificarStock(stockAccVehic[0]);
-                        break;
-                    case 2:
-                        verificarStock(stockAccVehic[1]);
-                        break;
-                    case 3:
-                        verificarStock(stockAccVehic[2]);
-                        break;
-                    case 4:
-                        break
-                    default:
-                        alert("Por favor ingrese un numero valido");
+                break;
+            case 2:
+                let op1 = Number(prompt("Que articulo desea comprar?: 1. Cable Espiral 0K051 _ 2. Cable Espiral EV06E _ 3. Cable Espiral 12070"));
+                while(op1 != 4) {
+                    switch(op1) {
+                        case 1:
+                            verificarStock(stockAccVehic[0]);
+                            break;
+                        case 2:
+                            verificarStock(stockAccVehic[1]);
+                            break;
+                        case 3:
+                            verificarStock(stockAccVehic[2]);
+                            break;
+                        case 4:
+                            break
+                        default:
+                            alert("Por favor ingrese una opcion valida");
+                    }
                 }
-            }
-        case 3:
-            let op2 = Number(prompt("Que articulo desea comprar?: 1. Cañon Ozonizador _ 2. Placa Ozonizadora _ 3. Tinta 5151"));
-            while(op2 != 4) {
-                switch(op2) {
-                    case 1:
-                        verificarStock(stockIndYOfi[0]);
-                        break;
-                    case 2:
-                        verificarStock(stockIndYOfi[1]);
-                        break;
-                    case 3:
-                        verificarStock(stockIndYOfi[2]);
-                        break;
-                    case 4:
-                        break
-                    default:
-                        alert("Por favor ingrese un numero valido");
+                break;
+            case 3:
+                let op2 = Number(prompt("Que articulo desea comprar?: 1. Cañon Ozonizador _ 2. Placa Ozonizadora _ 3. Tinta 5151"));
+                while(op2 != 4) {
+                    switch(op2) {
+                        case 1:
+                            verificarStock(stockIndYOfi[0]);
+                            break;
+                        case 2:
+                            verificarStock(stockIndYOfi[1]);
+                            break;
+                        case 3:
+                            verificarStock(stockIndYOfi[2]);
+                            break;
+                        case 4:
+                            break
+                        default:
+                            alert("Por favor ingrese una opcion valida");
+                    }
                 }
-            }
-        case 4:
-            break;
-        default:
-            alert("Ingrese un numero valido");
+                break;
+            case 4:
+                break;
+            default:
+                alert("Ingrese una opcion valida");
+        }
     }
 }
 
+compra();
